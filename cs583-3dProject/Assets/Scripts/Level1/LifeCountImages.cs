61c0f18632b70d0e533e9d73e8c9af8b9bff5e62
Caroline Forsythe <cforsythe1160@sdsu.edu>
2025-12-11 10:37:52 -0800

    life display now functioning

---
+++ cs583-3dProject/Assets/Scripts/Level1/LifeCountImages.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCountImages : MonoBehaviour{
+    public Image[] heartImages; // array of 3 hearts
+    public Sprite fullHeartSprite;
+    public Sprite emptyHeartSprite;
+
+    void Start()
+    {
+        UpdateHearts();
+    }
+
+    void Update()
+    {
+        // update hearts to reflect current lives
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        //loop through all hearts and update based on lives remaining
+        for (int i = 0; i < heartImages.Length; i++)
+        {
+            // hearts fill from left to right
+            if (PlayerController.numLivesLeft > i)
+            {
+                heartImages[i].sprite = fullHeartSprite;
+            }
+            else
+            {
+                heartImages[i].sprite = emptyHeartSprite;
+            }
+        }
+    }
+}
